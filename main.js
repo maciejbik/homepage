16fd9849153755e1e42f29c56f14268f6b36488f
Maciej Bik <maciek.bik@op.pl>
2021-04-06 19:03:14 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,4 @@
+alert("Czy wiedziałeś, że leworęczne osoby mają większą szanse na ukończenie egzaminu na czas niż ci, którzy umarli przy porodzie?")
+const imie = "Maciej"
+const wiek = 17
+console.log(`Nazywam się ${imie}} i mam ${wiek} lat`);
